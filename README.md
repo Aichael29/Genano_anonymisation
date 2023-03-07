d3b3a4be7f5713cfcc0ee603548ee5040f42031e
aichael29 <aichaelfelchaoui29@gmail.com>
2023-03-07 01:01:09 -0800

    readme

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # projetData_Inwi
+
+##### Le code fourni est un script Python qui permet d'anonymiser une colonne d'un fichier de données et de dé-anonymiser cette colonne par la suite en utilisant une technique de hachage.
+
+### Générer une clé aléatoire de 64 caractères en utilisant SHA-512
+
+def generate_secret_key():
+    secret_key = hashlib.sha512(os.urandom(64)).hexdigest()
+    return secret_key
+
+   ####    Test de la fonction
+
+secret_key = generate_secret_key()
+print(secret_key)
+
+Ce code va générer une clé aléatoire de 128 caractères (64 octets) en utilisant SHA-512 et l'afficher à l'écran. Vous pouvez exécuter ce code plusieurs fois pour générer des clés secrètes différentes à chaque fois.
+
+
+### Fonction pour nettoyer le hachage en renvoyant les 20 premiers caractères
+
+Ce code utilise la fonction de hachage SHA-512 pour anonymiser une colonne "ID" d'un DataFrame Pandas. La fonction "cleanhash" nettoie le hachage en ne gardant que les 20 premiers caractères. La méthode "apply" est utilisée pour appliquer cette fonction à chaque ligne de la colonne "ID" du DataFrame.
+La sortie du premier "print" montre le DataFrame original avec la colonne "ID" anonymisée en utilisant la fonction de hachage SHA-512. La sortie du deuxième "print" montre le DataFrame anonymisé ré-anonymisé à l'aide de la fonction de hachage SHA-512.
+
+### Fonction pour anonymiser une colonne dans un dataframe
+
+La fonction anonymize prend en entrée un dataframe df et une colonne column et ajoute une nouvelle colonne avec le nom column_hash qui contient les valeurs de la colonne column hachées à l'aide de la fonction cleanhash. Elle renvoie le dataframe avec la nouvelle colonne ajoutée.
+
+### Fonction pour déanonymiser une colonne dans un dataframe
+
+La fonction deanonymize(df, column) prend en entrée un dataframe et le nom de la colonne à déanonymiser. Elle applique la même fonction de hachage pour inverser l'anonymisation en utilisant la colonne de hachage et renvoie le dataframe déanonymisé en supprimant la colonne de hachage.
+
--- main.py
+++ main.py
@@ -1,83 +1,69 @@
-import hashlib
 import os
-
 import pandas as pd
-from hashlib import sha256
-from os import urandom
+import hashlib
 
-
-#Génère une clé secrète aléatoire pour le hachage
+# Générer une clé aléatoire de 64 caractères en utilisant SHA-512
 def generate_secret_key():
-    return sha256(urandom(64)).hexdigest()
+    secret_key = hashlib.sha512(os.urandom(64)).hexdigest()
+    return secret_key
 
-# test de la fonction
-secret_key = generate_secret_key()
-print(secret_key)
-
-
-
-#Clé secrète pour le hachage
-seckey = "y3HsVaFY9Uj<\C#!nMnK,%q=F?dR4WA|s(bwisfcU<q.P&L"
-
-#Dossier d'entrée et de sortie pour les fichiers
-folder = './'
-folderin = folder
-folderout = folder + "out/"
-
-#Fonction pour nettoyer le hachage en renvoyant les 20 premiers caractères
+# Fonction pour nettoyer le hachage en renvoyant les 20 premiers caractères
 def cleanhash(s):
     return hashlib.sha512(str(s).encode()).hexdigest()[:20]
 
-#Exemple d'utilisation
-df = pd.DataFrame({'ID': [1, 2, 3], 'Name': ['Alice', 'Bob', 'Charlie']})
-df_anonymized = df.copy()
-df_anonymized['ID'] = df['ID'].apply(cleanhash)
-print(df_anonymized)
-
-df_deanonymized = df_anonymized.copy()
-df_deanonymized['ID'] = df_anonymized['ID'].apply(lambda x: hashlib.sha512(str(x).encode()).hexdigest()[:20])
-print(df_deanonymized)
-
-#Fonction pour anonymiser une colonne dans un dataframe
+# Fonction pour anonymiser une colonne dans un dataframe
 def anonymize(df, column):
     df[column+'_hash'] = df[column].apply(cleanhash)
     return df
 
-#Fonction pour déanonymiser une colonne dans un dataframe
+# Fonction pour déanonymiser une colonne dans un dataframe
 def deanonymize(df, column):
-# Réappliquer la même fonction de hachage pour inverser l'anonymisation
+    # Réappliquer la même fonction de hachage pour inverser l'anonymisation
     df[column] = df[column+'_hash'].apply(lambda x: hashlib.sha512(str(x).encode()).hexdigest()[:20])
     return df.drop(columns=[column+'_hash'])
 
-#Exemple d'utilisation :
+# Clé secrète pour le hachage
+seckey = "y3HsVaFY9Uj<\C#!nMnK,%q=F?dR4WA|s(bwisfcU<q.P&L"
+
+# Dossier d'entrée et de sortie pour les fichiers
+folder = './'
+folderin = folder
+folderout = folder + "out/"
+
+# Exemple d'utilisation
+df = pd.DataFrame({'ID': [1, 2, 3], 'Name': ['Alice', 'Bob', 'Charlie']})
+df_anonymized = anonymize(df.copy(), 'ID')
+df_deanonymized = deanonymize(df_anonymized.copy(), 'ID')
+
+# Nom du fichier d'entrée
 filename = 'input.csv' # peut être n'importe quel type de fichier, par exemple .txt, .csv, .xlsx, etc.
 
-#Chemin d'accès au fichier d'entrée
+# Chemin d'accès au fichier d'entrée
 filepath = os.path.join(folderin, filename)
 
-#Charger le fichier d'entrée dans un dataframe
+# Charger le fichier d'entrée dans un dataframe
 df_input = pd.read_csv(filepath, header=0) # supposons que le fichier d'entrée a une ligne d'en-tête
 
-#Anonymiser la colonne 'ID' du dataframe d'entrée
+# Anonymiser la colonne 'ID' du dataframe d'entrée
 df_anonymized = anonymize(df_input, 'ID')
 
-#Nom du fichier de sortie pour les données anonymisées
+# Nom du fichier de sortie pour les données anonymisées
 output_filename = os.path.splitext(filename)[0] + '_anonymized.xlsx'
 
-#Chemin d'accès pour le fichier de sortie
+# Chemin d'accès pour le fichier de sortie
 output_filepath = os.path.join(folderout, output_filename)
 
-#Exporter les données anonymisées dans un fichier Excel
+# Exporter les données anonymisées dans un fichier Excel
 df_anonymized.to_excel(output_filepath, sheet_name='hash', index=False)
 
-#Inverser l'anonymisation pour retrouver les données originales
+# Inverser l'anonymisation pour retrouver les données originales
 df_deanonymized = deanonymize(df_anonymized, 'ID')
 
-#Nom du fichier de sortie pour les données déanonymisées
+# Nom du fichier de sortie pour les données déanonymisées
 output_filename = os.path.splitext(filename)[0] + '_deanonymized.xlsx'
 
-#Chemin d'accès pour le fichier de sortie
+# Chemin d'accès pour le fichier de sortie
 output_filepath = os.path.join(folderout, output_filename)
 
-#Exporter les données déanonymisées dans un fichier Excel
+# Exporter les données déanonymisées dans un fichier Excel
 df_deanonymized.to_excel(output_filepath, sheet_name='hash', index=False)
