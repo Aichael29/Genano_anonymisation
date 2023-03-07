859fc7e26e9e72b9512ce77a3e08f7a325369663
aichael29 <aichaelfelchaoui29@gmail.com>
2023-03-07 03:56:47 -0800

    separat°

---
+++ anonymisation.py
@@ -0,0 +1,56 @@
+import os
+import pandas as pd
+import hashlib
+
+#Générer une clé aléatoire de 64 caractères en utilisant SHA-512
+def generate_secret_key():
+    secret_key = hashlib.sha512(os.urandom(64)).hexdigest()
+    return secret_key
+
+#Fonction pour nettoyer le hachage en renvoyant les 20 premiers caractères
+def cleanhash(s):
+    return hashlib.sha512(str(s).encode()).hexdigest()[:20]
+
+#Fonction pour anonymiser une colonne dans un dataframe
+def anonymize(df):
+    for column in df.columns:
+        df[column+'_hash'] = df[column].apply(cleanhash)
+    return df
+
+#Clé secrète pour le hachage
+seckey = "y3HsVaFY9Uj<\C#!nMnK,%q=F?dR4WA|s(bwisfcU<q.P&L"
+
+#Dossier d'entrée et de sortie pour les fichiers
+folder = 'C:/Users/Lenovo/Pictures/'
+folderin = folder
+folderout = folder + "out/"
+
+#Nom du fichier d'entrée
+filename = 'input.csv'
+
+#Chemin d'accès au fichier d'entrée
+filepath = os.path.join(folderin, filename)
+
+#Vérifier le type de fichier d'entrée
+file_ext = os.path.splitext(filename)[1]
+if file_ext == ".xlsx":
+    # Charger le fichier Excel d'entrée dans un dataframe
+    df_input = pd.read_excel(filepath)
+elif file_ext == ".csv":
+    # Charger le fichier CSV d'entrée dans un dataframe
+    df_input = pd.read_csv(filepath)
+else:
+    print("Extension de fichier non prise en charge.")
+    exit()
+
+#Anonymiser les données
+df_anonymized = anonymize(df_input.copy())
+
+#Nom du fichier de sortie pour les données anonymisées
+output_filename = os.path.splitext(filename)[0] + '_anonymized.csv'
+
+#Chemin d'accès pour le fichier de sortie
+output_filepath = os.path.join(folderout, output_filename)
+
+#Exporter les données anonymisées dans un fichier CSV
+df_anonymized.to_csv(output_filepath, index=False)
---
+++ deanonymisation.py
@@ -0,0 +1,52 @@
+import os
+import pandas as pd
+import hashlib
+
+# Générer une clé aléatoire de 64 caractères en utilisant SHA-512
+def generate_secret_key():
+    secret_key = hashlib.sha512(os.urandom(64)).hexdigest()
+    return secret_key
+
+# Fonction pour nettoyer le hachage en renvoyant les 20 premiers caractères
+def cleanhash(s):
+    return hashlib.sha512(str(s).encode()).hexdigest()[:20]
+
+# Fonction pour déanonymiser une colonne dans un dataframe
+def deanonymize(df):
+    for column in df.columns:
+        if '_hash' in column:
+            column_name = column.replace('_hash', '')
+            # Réappliquer la même fonction de hachage pour inverser l'anonymisation
+            df[column_name] = df[column].apply(lambda x: hashlib.sha512(str(x).encode()).hexdigest()[:20])
+    return df.drop(columns=[col for col in df.columns if '_hash' in col])
+
+# Dossier d'entrée et de sortie pour les fichiers
+folder = 'C:/Users/Lenovo/Pictures/'
+folderin = folder
+folderout = folder + "out/"
+
+# Nom du fichier d'entrée
+filename = 'input.xlsx'
+
+# Chemin d'accès au fichier d'entrée
+filepath = os.path.join(folderin, filename)
+
+# Charger le fichier d'entrée dans un dataframe
+if filepath.endswith('.csv'):
+    df_input = pd.read_csv(filepath, encoding='iso-8859-1')
+elif filepath.endswith('.xlsx'):
+    df_input = pd.read_excel(filepath)
+else:
+    raise ValueError("Type de fichier non supporté")
+
+# Déanonymiser les données
+df_deanonymized = deanonymize(df_input.copy())
+
+# Nom du fichier de sortie pour les données déanonymisées
+output_filename = os.path.splitext(filename)[0] + '_deanonymized.xlsx'
+
+# Chemin d'accès pour le fichier de sortie
+output_filepath = os.path.join(folderout, output_filename)
+
+# Exporter les données déanonymisées dans un fichier xlsx
+df_deanonymized.to_excel(output_filepath, index=False)
