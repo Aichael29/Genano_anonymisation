4af93448de37ad1060e901d3c956fb9a87d87fb2
aichael29 <aichaelfelchaoui29@gmail.com>
2023-03-08 12:10:27 +0100

    updated

--- deanonymisation.py
+++ deanonymisation.py
@@ -1,22 +1,32 @@
-'''import pandas as pd
+import pandas as pd
+import hashlib
 
+# Clé secrète utilisée pour l'anonymisation
+seckey = "y3HsVaFY9Uj<\C#*!nMnK,*%q=F?dR4WA|s(bwisfcU<q.P\&L"
 
-def deanonymize_excel(anonymized_filename, original_filename):
-    # Charger les fichiers Excel
-    df_anonymized = pd.read_excel(anonymized_filename)
-    df_original = pd.read_excel(original_filename)
+# Fonction pour récupérer l'ID original à partir du hash
+def get_id(hash):
+    # Essayer les 1 000 000 premiers entiers en les hashant avec la clé secrète et en comparant les 20 premiers
+    # caractères du hash avec le hash fourni. Si une correspondance est trouvée, renvoyer l'entier correspondant.
+    for i in range(1000000):
+        if hashlib.sha512(bytearray(str(i).strip()+seckey, "utf8")).hexdigest()[:20] == hash:
+            return i
+    # Si aucune correspondance n'est trouvée, renvoyer None.
+    return None
 
-    # Extraire les colonnes qui ont été anonymisées
-    cols_to_anonymize = [col for col in df_anonymized.columns if col.startswith("anon_")]
+# Fonction pour désanonymiser une colonne
+def dehash_column(df, col_name):
+    if df[col_name].dtype == pd.np.object or df[col_name].dtype == pd.np.str:
+        # Si la colonne contient des chaînes de caractères, appliquer la fonction get_id() à chaque élément de la colonne
+        # pour récupérer l'ID original correspondant au hash
+        df[col_name] = pd.Series([get_id(df[col_name][i]) for i in range(len(df[col_name]))])
+    else:
+        # Si la colonne contient des nombres, appliquer la fonction get_id() à chaque élément de la colonne
+        # pour récupérer l'ID original correspondant au hash
+        df[col_name] = df[col_name].apply(get_id)
 
-    # Remplacer les valeurs anonymisées par les valeurs originales
-    for col in cols_to_anonymize:
-        df_anonymized[col] = df_anonymized[col].apply(
-            lambda x: df_original.loc[df_original[col].astype(str) == x, col].iloc[0])
-
-    # Enregistrer le fichier Excel déanonymisé
-    deanonymized_filename = f"deanonymized_{anonymized_filename}"
-    df_anonymized.to_excel(deanonymized_filename, index=False)
-
-    return deanonymized_filename
-'''
+# Fonction pour désanonymiser plusieurs colonnes
+def dehash_columns(df, col_names):
+    for col_name in col_names:
+        # Appliquer la fonction dehash_column() à chaque colonne indiquée dans la liste col_names
+        dehash_column(df, col_name)
--- main.py
+++ main.py
@@ -1,25 +1,39 @@
 import pandas as pd
 from anonymisation import anonymize
+from deanonymisation import dehash_columns
 
 # Demander à l'utilisateur les chemins d'entrée et de sortie
 folderin = input("Entrez le chemin du dossier d'entrée: ")
 folderout = input("Entrez le chemin du dossier de sortie: ")
 
-# Lecture du fichier Excel d'entrée
-input_file = input("Entrez le nom du fichier d'entrée: ")
-df_input = pd.read_excel(folderin + "/" + input_file, header=0)
+# Lecture du fichier Excel original
+input_file = input("Entrez le nom du fichier Excel original: ")
+df_input = pd.read_excel(folderin+"/"+input_file, header=0)
 
-# Demander à l'utilisateur les colonnes à anonymiser
-cols = input(
-    "Entrez les noms de colonnes séparés par des virgules (laissez vide pour anonymiser toutes les colonnes): ")
-if cols == "":
-    cols = df_input.columns
-else:
-    cols = cols.split(",")
+# Demander à l'utilisateur les noms des colonnes à anonymiser
+cols = input("Entrez les noms des colonnes séparés par des virgules à anonymiser: ")
+cols = cols.split(",")
 
-# Anonymisation des colonnes sélectionnées
-df_output = anonymize(df_input, cols)
+# Anonymiser les colonnes sélectionnées
+df_anon = anonymize(df_input, cols)
 
-# Écrire le fichier Excel de sortie avec les valeurs anonymisées
+# Écrire le fichier Excel anonymisé
 output_file = input("Entrez le nom du fichier de sortie: ")
-df_output.to_excel(folderout + "/" + output_file, sheet_name='hash', index=False)
+df_anon.to_excel(folderout+"/"+output_file, sheet_name='anonyme', index=False)
+
+# Demander à l'utilisateur les noms des colonnes à désanonymiser
+cols = input("Entrez les noms des colonnes séparés par des virgules à désanonymiser: ")
+cols = cols.split(",")
+
+# Lecture du fichier Excel anonymisé
+input_file = output_file
+df_input = pd.read_excel(folderout+"/"+input_file, header=0)
+
+# Désanonymiser les colonnes sélectionnées
+dehash_columns(df_input, cols)
+
+# Écrire le fichier Excel désanonymisé
+output_file = "deanonymise_" + output_file
+df_input.to_excel(folderout+"/"+output_file, sheet_name='deanonymise', index=False)
+
+print("Opération terminée avec succès !")
