0509b73f62317466ad1c8a41fd3d270545f182f1
aichael29 <aichaelfelchaoui29@gmail.com>
2023-03-09 17:22:14 +0100

    chiffrement des string

--- crypt.py
+++ crypt.py
@@ -1,25 +1,25 @@
 import pandas as pd
 from Cryptodome.Cipher import AES
 from Cryptodome.Util.Padding import pad
-import os
 
-# Étape 1 : Charger le fichier Excel en utilisant pandas
+# Load the Excel file into a pandas dataframe
 df = pd.read_excel('input.xlsx')
 
-# Étape 2 : Chiffrer la colonne souhaitée en utilisant AES
-iv = os.urandom(16) # vecteur d'initialisation aléatoire
-key = b'maclefsecrete16b' # clé de chiffrement partagée
-cipher = AES.new(key, AES.MODE_CBC, iv=iv) # initialisation de l'objet AES
+# Choose the column you want to encrypt
+column_to_encrypt = 'phone number'
 
-colonne_chiffree = cipher.encrypt(pad(df['ID'].to_string(index=False).encode(), AES.block_size)) # chiffrement de la colonne sensible
+# Define a function to encrypt the values in the column
+def aes_encrypt(value, key):
+    cipher = AES.new(key.encode(), AES.MODE_ECB)
+    padded_value = pad(value.encode(), AES.block_size)
+    encrypted_value = cipher.encrypt(padded_value)
+    return encrypted_value.hex()
 
-# Étape 4 : Ajouter la colonne chiffrée au DataFrame
-df['colonne_chiffree'] = [colonne_chiffree]
+# Set a key for encryption
+key = 'my_secret_key123'
 
-# Étape 5 : Supprimer la colonne sensible du DataFrame
-df = df.drop(columns=['ID'])
+# Apply the encryption function to the chosen column
+df[column_to_encrypt] = df[column_to_encrypt].apply(lambda x: aes_encrypt(x, key))
 
-# Étape 6 : Enregistrer le DataFrame chiffré dans un nouveau fichier Excel
-writer = pd.ExcelWriter('fichier_excel_chiffre.xlsx')
-df.to_excel(writer, index=False)
-writer.save()
+# Save the encrypted file
+df.to_excel('output_file.xlsx', index=False)
--- decrypt.py
+++ decrypt.py
@@ -1,22 +1,25 @@
 import pandas as pd
 from Cryptodome.Cipher import AES
 from Cryptodome.Util.Padding import unpad
-import binascii
 
-# Étape 8 : Déchiffrer la colonne souhaitée en utilisant la même clé de chiffrement partagée et le même vecteur d'initialisation
-df = pd.read_excel('fichier_excel_chiffre.xlsx')
-key = b'maclefsecrete16b' # clé de chiffrement partagée
-iv = df['colonne_chiffree'][0][:16] # extraire le vecteur d'initialisation du fichier Excel
-cipher = AES.new(key, AES.MODE_CBC, iv=iv) # initialisation de l'objet AES
-encrypted_data = df['colonne_chiffree'][0]
-colonne_dechiffree = unpad(cipher.decrypt(encrypted_data), AES.block_size).decode() # déchiffrement de la colonne chiffrée
+# Load the encrypted Excel file into a pandas dataframe
+df = pd.read_excel('output_file.xlsx')
 
-# Étape 9 : Déchiffrer l'ensemble du fichier Excel
-iv = encrypted_data[:16] # extraire le vecteur d'initialisation du fichier Excel
-cipher = AES.new(key, AES.MODE_CBC, iv=iv) # initialisation de l'objet AES
-decrypted_data = cipher.decrypt(encrypted_data)
-df['colonne_sensible'] = unpad(decrypted_data, AES.block_size).decode() # déchiffrement de la colonne chiffrée
-df = df.drop(columns=['colonne_chiffree']) # supprimer la colonne chiffrée du DataFrame
-writer = pd.ExcelWriter('fichier_excel_dechiffre.xlsx') # enregistrer le DataFrame déchiffré dans un nouveau fichier Excel
-df.to_excel(writer, index=False)
-writer.save()
+# Choose the column you want to decrypt
+column_to_decrypt = 'phone number'
+
+# Define a function to decrypt the values in the column
+def aes_decrypt(value, key):
+    cipher = AES.new(key.encode(), AES.MODE_ECB)
+    decrypted_value = cipher.decrypt(bytes.fromhex(value))
+    unpadded_value = unpad(decrypted_value, AES.block_size)
+    return unpadded_value.decode()
+
+# Set the key for decryption
+key = 'my_secret_key123'
+
+# Apply the decryption function to the chosen column
+df[column_to_decrypt] = df[column_to_decrypt].apply(lambda x: aes_decrypt(x, key))
+
+# Save the decrypted file
+df.to_excel('decrypted_file.xlsx', index=False)
