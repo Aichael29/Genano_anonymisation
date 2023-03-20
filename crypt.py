2d256943aab07374918b4782e0708ebd812dca56
aichael29 <aichaelfelchaoui29@gmail.com>
2023-03-20 15:09:58 +0000

    fichier configu

--- crypt.py
+++ crypt.py
@@ -1,32 +1,34 @@
 import pandas as pd
 from Cryptodome.Cipher import AES
 from Cryptodome.Util.Padding import pad
+import configparser
 
-# Charger le fichier Excel dans un dataframe Pandas
-input_file = input("Entrez le nom du fichier d'entrée (par ex. 'input.xlsx') : ")
-df = pd.read_excel(input_file)
+# Charger le fichier de configuration
+config = configparser.ConfigParser()
+config.read('configuration.conf')
 
-# Demander à l'utilisateur les colonnes à crypter
-columns_to_encrypt = input("Entrez le nom de la ou des colonnes à crypter, séparées par des virgules : ").split(',')
-
-# Demander à l'utilisateur la clé de chiffrement
-key = input("Entrez la clé de chiffrement de 16 caractères : ")
+# Récupérer les paramètres de configuration
+key = config['operation']['cle_chiffrement']
+vector = config['operation']['vector']
+columns_to_encrypt = config['operation']['colonnes'].split(',')
+input_file = config['fileinfo']['fichier_entree']
+output_file = config['fileinfo']['fichier_sortie']
 
 # Définir une fonction pour crypter les valeurs d'une colonne
-def aes_encrypt(value, key):
-    cipher = AES.new(key.encode(), AES.MODE_ECB)
+def aes_encrypt(value, key, vector):
+    cipher = AES.new(key.encode(), AES.MODE_CBC, iv=vector.encode())
     if isinstance(value, int):  # vérifier si la valeur est un entier
         value = str(value)  # convertir l'entier en chaîne de caractères
     padded_value = pad(value.encode(), AES.block_size)
     encrypted_value = cipher.encrypt(padded_value)
     return encrypted_value.hex()
 
+# Charger le fichier Excel dans un dataframe Pandas
+df = pd.read_excel(input_file)
+
 # Appliquer la fonction de chiffrement à chaque colonne choisie
 for col in columns_to_encrypt:
-    df[col] = df[col].apply(lambda x: aes_encrypt(x, key))
+    df[col] = df[col].apply(lambda x: aes_encrypt(x, key, vector))
 
 # Enregistrer le fichier Excel crypté
-output_file = input("Entrez le nom du fichier de sortie (par ex. 'output.xlsx') : ")
 df.to_excel(output_file, index=False)
-
-print("Cryptage terminé !")
