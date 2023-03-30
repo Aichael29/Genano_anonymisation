6f1af939000521f3ac8992b8a17a3ed294daaa54
aichael29 <aichaelfelchaoui29@gmail.com>
2023-03-30 12:23:04 +0000

    annony.py

---
+++ annony.py
@@ -0,0 +1,44 @@
+import hashlib
+from Cryptodome.Cipher import AES
+from Cryptodome.Util.Padding import pad, unpad
+
+
+# Définir une fonction pour crypter les valeurs d'une colonne avec AES en mode CBC ou ECB
+def aes_encrypt(value, key, mode_chiffrement, iv=None):
+    if mode_chiffrement == 'CBC' and iv is None:
+        raise ValueError("Le vecteur d'initialisation doit être spécifié pour le mode CBC.")
+    if mode_chiffrement == 'CBC':
+        cipher = AES.new(key.encode(), AES.MODE_CBC, iv.encode())
+    else:
+        cipher = AES.new(key.encode(), AES.MODE_ECB)
+    if isinstance(value, str):
+        padded_value = pad(value.encode(), AES.block_size)
+    elif isinstance(value, int):
+        padded_value = pad(str(value).encode(), AES.block_size)
+    elif isinstance(value, float):
+        padded_value = pad(str(value).encode(), AES.block_size)
+    else:
+        raise ValueError("Type de données non pris en charge.")
+    encrypted_value = cipher.encrypt(padded_value)
+    return encrypted_value.hex()
+# Define decryption function
+def aes_decrypt(value, key, mode_chiffrement, iv=None):
+    if mode_chiffrement == 'CBC' and iv is None:
+        raise ValueError("Le vecteur d'initialisation doit être spécifié pour le mode CBC.")
+    if mode_chiffrement == 'ECB' and iv is not None:
+        raise ValueError("IV n'est pas utile pour le mode ECB.")
+    if mode_chiffrement == 'CBC':
+        cipher = AES.new(key.encode(), AES.MODE_CBC, iv.encode())
+    else:
+        cipher = AES.new(key.encode(), AES.MODE_ECB)
+    encrypted_value = bytes.fromhex(value)
+    decrypted_value = unpad(cipher.decrypt(encrypted_value), AES.block_size)
+    if isinstance(decrypted_value, bytes):
+        return decrypted_value.decode()
+    else:
+        return decrypted_value
+
+
+# Définir une fonction pour hasher les valeurs d'une colonne avec SHA256
+def sha256_hash(value):
+    return hashlib.sha256(value.encode()).hexdigest()
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import configparser
 import sys
-import subprocess
 import time
+from annony import aes_encrypt , aes_decrypt, sha256_hash
+import pandas as pd
 
 start=time.time()
 # Lecture du fichier de configuration
@@ -11,16 +12,62 @@ config.read('configuration.conf')
 # Opération à effectuer
 operation = config['Operations']['operation']
 
-# Exécuter le fichier Python correspondant à l'opération
-if operation == 'chiffrement':
-    subprocess.run(['python', 'crypt.py'])
-elif operation == 'dechiffrement':
-    subprocess.run(['python', 'decrypt.py'])
-elif operation == 'hashage':
-    subprocess.run(['python', 'hash.py'])
+
+# Mode de chiffrement (ECB ou CBC)
+mode_chiffrement = config['Operations']['mode_chiffrement']
+
+# Colonnes à crypter
+colonnes = config['Operations'].get('colonnes', None)
+if colonnes is None:
+    colonnes = []
+
+# Clé de chiffrement
+cle_chiffrement = config['Operations']['cle_chiffrement']
+
+# Vecteur d'initialisation
+vecteur = config['Operations']['vector']
+
+# Charger le fichier Excel dans un dataframe Pandas
+fichier_entree = config['fileinfo']['fichier_entree']
+df = pd.read_excel(fichier_entree)
+
+# Vérifier si la clé a la bonne taille
+if len(cle_chiffrement) != 16:
+    print("La clé doit être de 16 caractères.")
+    exit()
+
+# Vérifier si le mode de chiffrement est valide
+if mode_chiffrement not in ('ECB', 'CBC'):
+    print("Le mode de chiffrement doit être soit ECB ou CBC.")
+    exit()
+
+# Vérifier si le vecteur d'initialisation a la bonne taille
+if mode_chiffrement == 'CBC' and len(vecteur) != 16:
+    print("Le vecteur d'initialisation doit être de 16 octets.")
+    exit()
+
+
+
+# Appliquer la fonction de decryptage à chaque colonne choisie
+if colonnes:
+    colonnes = colonnes.split(',')
 else:
-    print("Opération non reconnue.")
-    sys.exit(1)
+    colonnes = df.columns.tolist()
+for col in colonnes:
+    if operation == 'chiffrement':
+        df[col] = df[col].apply(lambda x: aes_encrypt(x, cle_chiffrement, mode_chiffrement,iv=vecteur if mode_chiffrement == 'CBC' else None))
+    elif operation == 'dechiffrement':
+        df[col] = df[col].apply(lambda x: aes_decrypt(x, cle_chiffrement, mode_chiffrement,iv=vecteur if mode_chiffrement == 'CBC' else None))
+    elif operation == 'hashage':
+        df[col] = df[col].apply(lambda x: sha256_hash(str(x)))
+    else:
+        print("Opération non reconnue.")
+        sys.exit(1)
+
+# Enregistrer le fichier Excel hashé
+fichier_sortie = config['fileinfo']['fichier_sortie']
+df.to_excel(fichier_sortie, index=False)
+
 
 end=time.time()
 print("xlsx généré en " + str(end - start) + " secondes")
