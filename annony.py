f9e97715b75740942d2e45c208383bd82fcf152b
Trajaa <rajaa.tiane1@gmail.com>
2023-04-03 14:53:23 +0000

    chiffrement/dechiffrement/hachage d'une cle json

--- annony.py
+++ annony.py
@@ -1,10 +1,23 @@
 import hashlib
+
+import pandas as pd
 from Cryptodome.Cipher import AES
 from Cryptodome.Util.Padding import pad, unpad
+import sys
 
-
+def get_keys(data):
+    keys=[]
+    for key, value in data.items():
+        keys.append(key)
+        if isinstance(value, dict):
+            keys.extend(get_keys(value))
+        elif isinstance(value, list):
+            for item in value:
+                if isinstance(item, dict):
+                    keys.extend(get_keys(item))
+    return keys
 # Définir une fonction pour crypter les valeurs d'une colonne avec AES en mode CBC ou ECB
-def aes_encrypt(value, key, mode_chiffrement=None, iv=None):
+def aes_encrypt(value, key, mode_chiffrement, iv=None):
     if mode_chiffrement == 'CBC' and iv is None:
         raise ValueError("Le vecteur d'initialisation doit être spécifié pour le mode CBC.")
     if mode_chiffrement == 'CBC':
@@ -17,9 +30,9 @@ def aes_encrypt(value, key, mode_chiffrement=None, iv=None):
         padded_value = pad(str(value).encode(), AES.block_size)
     elif isinstance(value, float):
         padded_value = pad(str(value).encode(), AES.block_size)
-    elif isinstance(value,bytes):
+    elif isinstance(value, bytes):
         padded_value = pad(value, AES.block_size)
-    elif isinstance(value,bytearray ):
+    elif isinstance(value, bytearray):
         padded_value = pad(bytes(value), AES.block_size)
     elif isinstance(value, memoryview):
         padded_value = pad(bytes(value), AES.block_size)
@@ -27,8 +40,10 @@ def aes_encrypt(value, key, mode_chiffrement=None, iv=None):
         raise ValueError("Type de données non pris en charge.")
     encrypted_value = cipher.encrypt(padded_value)
     return encrypted_value.hex()
+
+# Définir une fonction pour décrypter les valeurs d'une colonne avec AES en mode CBC ou ECB
 # Define decryption function
-def aes_decrypt(value, key, mode_chiffrement=None, iv=None):
+def aes_decrypt(value, key, mode_chiffrement, iv=None):
     if mode_chiffrement == 'CBC' and iv is None:
         raise ValueError("Le vecteur d'initialisation doit être spécifié pour le mode CBC.")
     if mode_chiffrement == 'ECB' and iv is not None:
@@ -37,14 +52,89 @@ def aes_decrypt(value, key, mode_chiffrement=None, iv=None):
         cipher = AES.new(key.encode(), AES.MODE_CBC, iv.encode())
     else:
         cipher = AES.new(key.encode(), AES.MODE_ECB)
-    encrypted_value = bytes.fromhex(value)
-    decrypted_value = unpad(cipher.decrypt(encrypted_value), AES.block_size)
-    if isinstance(decrypted_value, bytes):
-        return decrypted_value.decode()
-    else:
-        return decrypted_value
-
+    decrypted_value = unpad(cipher.decrypt(bytes.fromhex(value)), AES.block_size)
+    return decrypted_value.decode('utf-8')
 
 # Définir une fonction pour hasher les valeurs d'une colonne avec SHA256
 def sha256_hash(value):
-    return hashlib.sha256(value.encode()).hexdigest()
+    if isinstance(value, bytes):
+        return hashlib.sha256(value).hexdigest()
+    elif isinstance(value, bytearray):
+        return hashlib.sha256(bytes(value)).hexdigest()
+    elif isinstance(value, memoryview):
+        return hashlib.sha256(bytes(value)).hexdigest()
+    elif hasattr(value, 'read'):
+        value_hash = hashlib.sha256()
+        while True:
+            chunk = value.read(4096)
+            if not chunk:
+                break
+            value_hash.update(chunk)
+        return value_hash.hexdigest()
+    else:
+        return hashlib.sha256(str(value).encode()).hexdigest()
+def encrypt_file(file_extension, fichier_entree, fichier_sortie ,operation,cle_chiffrement, mode_chiffrement, iv=None, colonnes=None):
+
+    # vérifier l'extension et exécuter le traitement approprié
+    if file_extension in ['.csv', '.txt','.xml','.json','.html']:
+        # ouvrir le fichier et lire chaque ligne
+        with open(fichier_entree, 'r') as file:
+            lines = []
+            for line in file:
+                # appliquer l'opération sur chaque ligne
+                if operation == 'chiffrement':
+                    line = aes_encrypt(line.strip(), cle_chiffrement, mode_chiffrement, iv=None)
+                elif operation == 'dechiffrement':
+                    line = aes_decrypt(line.strip(), cle_chiffrement, mode_chiffrement, iv=None)
+                elif operation == 'hashage':
+                    line = sha256_hash(line.strip())
+                else:
+                    print("Opération non reconnue.")
+                    sys.exit(1)
+                lines.append(line)
+
+        # écrire les lignes modifiées dans un nouveau fichier
+        with open(fichier_sortie, 'w') as encrypted_file:
+            for line in lines:
+                encrypted_file.write(str(line) + '\n')
+
+
+
+    elif file_extension == '.xlsx':
+        df = pd.read_excel(fichier_entree, engine='openpyxl')
+
+        # Appliquer la fonction de decryptage à chaque colonne spécifiée
+        if colonnes:
+            for col in colonnes:
+                if operation == 'chiffrement':
+                    df[col] = df[col].apply(lambda x: aes_encrypt(str(x), cle_chiffrement, mode_chiffrement, iv))
+                elif operation == 'dechiffrement':
+                    df[col] = df[col].apply(lambda x: aes_decrypt(str(x), cle_chiffrement, mode_chiffrement, iv))
+                elif operation == 'hashage':
+                    df[col] = df[col].apply(lambda x: sha256_hash(str(x)))
+
+        # écrire le dataframe modifié dans un nouveau fichier excel
+        df.to_excel(fichier_sortie, index=False)
+    elif file_extension in ['.jpg', '.jpeg', '.png', '.gif','.bmp','.docx']:
+        # Ouvrir le fichier file
+        with open(fichier_entree, 'rb') as f:
+            file = f.read()
+        # Appliquer l'opération sur l'file
+            if operation == 'chiffrement':
+                # Chiffrer l'file avec AES
+                image_encrypted = aes_encrypt(file, cle_chiffrement, mode_chiffrement, iv=None)
+                with open(fichier_sortie, 'wb') as f:
+                    f.write(image_encrypted)
+            elif operation == 'dechiffrement':
+                # Déchiffrer l'file avec AES
+                image_decrypted = aes_decrypt(file, cle_chiffrement, mode_chiffrement, iv=None)
+                with open(fichier_sortie, 'wb') as f:
+                    f.write(image_decrypted)
+            elif operation == 'hashage':
+                # Calculer le hash SHA-256 de l'file
+                image_hash = sha256_hash(file)
+                with open(fichier_sortie, 'wb') as f:
+                    f.write(image_hash.encode())
+    else:
+                    print("Opération non reconnue.")
+                    sys.exit(1)
--- main.py
+++ main.py
@@ -1,41 +1,33 @@
 import configparser
-import csv
 import json
 import os
-import sys
 import time
-from annony import aes_encrypt, aes_decrypt, sha256_hash
+from annony import *
 import pandas as pd
-
 start=time.time()
 # Lecture du fichier de configuration
 config = configparser.ConfigParser()
 config.read('configuration.conf')
-
 # Opération à effectuer
 operation = config['Operations']['operation']
 
+#type=config['Operations']['filetype']
 
 # Mode de chiffrement (ECB ou CBC)
 mode_chiffrement = config['Operations']['mode_chiffrement']
-
 # Colonnes à crypter
 colonnes = config['Operations'].get('colonnes', None)
 if colonnes is None:
     colonnes = []
-
+print(colonnes)
 # Clé de chiffrement
 cle_chiffrement = config['Operations']['cle_chiffrement']
-
 # Vecteur d'initialisation
 vecteur = config['Operations']['vector']
-
 # Charger le fichier Excel dans un dataframe Pandas
 fichier_entree = config['fileinfo']['fichier_entree']
-file_extension = os.path.splitext(fichier_entree)[1].lower()
 # Charger le fichier Excel dans un dataframe Pandas
 fichier_sortie = config['fileinfo']['fichier_sortie']
-
 
 type=os.path.splitext(fichier_entree)[1].lower()
 
@@ -43,112 +35,138 @@ type=os.path.splitext(fichier_entree)[1].lower()
 if len(cle_chiffrement) != 16:
     print("La clé doit être de 16 caractères.")
     exit()
-
 # Vérifier si le mode de chiffrement est valide
 if mode_chiffrement not in ('ECB', 'CBC'):
     print("Le mode de chiffrement doit être soit ECB ou CBC.")
     exit()
-
 # Vérifier si le vecteur d'initialisation a la bonne taille
 if mode_chiffrement == 'CBC' and len(vecteur) != 16:
     print("Le vecteur d'initialisation doit être de 16 octets.")
     exit()
-def encrypt_file(file_extension, fichier_entree, fichier_sortie ,cle_chiffrement, mode_chiffrement, iv=None, colonnes=None):
 
-        # vérifier l'extension et exécuter le traitement approprié
-     if file_extension in ['.csv', '.txt']:
-         # ouvrir le fichier et lire chaque ligne
-            with open(fichier_entree, 'r') as file:
+
+def encrypt_file(file_extension, fichier_entree, fichier_sortie,operation,cle_chiffrement, mode_chiffrement, iv=None, colonnes=None):
+    if file_extension == '.json':
+        # ouvrir le fichier et lire chaque ligne
+        print(file_extension)
+        with open(fichier_entree, 'r') as file:
+            data = json.load(file)
+
+        keys = get_keys(data)
+        # appliquer l'opération sur chaque ligne
+        if operation == 'chiffrement':
+            for key in keys:
+                if colonnes and key not in colonnes:
+                    continue
+                if mode_chiffrement == 'ECB':
+                    data[key] = aes_encrypt(data[key], cle_chiffrement, mode_chiffrement)
+                else:
+                    data[key] = aes_encrypt(data[key], cle_chiffrement, mode_chiffrement, iv)
+        elif operation == 'dechiffrement':
+            for key in keys:
+                if colonnes and key not in colonnes:
+                    continue
+                if mode_chiffrement == 'ECB':
+                    data[key] = aes_decrypt(data[key], cle_chiffrement, mode_chiffrement)
+                else:
+                    data[key] = aes_decrypt(data[key], cle_chiffrement, mode_chiffrement, iv)
+        elif operation == 'hashage':
+            for key in keys:
+                if colonnes and key not in colonnes:
+                    continue
+                data[key] = sha256_hash(data[key])
+        else:
+            print("Opération non reconnue.")
+            sys.exit(1)
+
+        # écrire les données modifiées dans un nouveau fichier
+        with open(fichier_sortie, 'w') as encrypted_file:
+            json.dump(data, encrypted_file, indent=4)
+
+
+        # ouvrir le fichier et lire chaque ligne
+    elif file_extension in ['.csv','.txt','.xml','.html']:
+             with open(fichier_entree, 'r') as file:
                 lines = []
                 for line in file:
                     # appliquer l'opération sur chaque ligne
                     if operation == 'chiffrement':
-                        line = aes_encrypt(line.strip(), cle_chiffrement, mode_chiffrement=None, iv=None)
+                        line = aes_encrypt(line.strip(), cle_chiffrement, mode_chiffrement, iv=None)
                     elif operation == 'dechiffrement':
-                        line = aes_decrypt(line.strip(), cle_chiffrement, mode_chiffrement=None, iv=None)
+                        line = aes_decrypt(line.strip(), cle_chiffrement, mode_chiffrement, iv=None)
                     elif operation == 'hashage':
                         line = sha256_hash(line.strip())
                     else:
                         print("Opération non reconnue.")
                         sys.exit(1)
                     lines.append(line)
-
             # écrire les lignes modifiées dans un nouveau fichier
-            with open(fichier_sortie, 'w') as encrypted_file:
+             with open(fichier_sortie, 'w') as encrypted_file:
                 for line in lines:
                     encrypted_file.write(line + '\n')
 
-     elif file_extension == '.json':
-            # ouvrir le fichier json et charger le contenu en un dictionnaire
-            with open(fichier_entree, 'r') as json_file:
-                json_dict = json.load(json_file)
 
-            # appliquer l'opération sur chaque valeur de chaque clé spécifiée
-            for key in json_dict:
-                if colonnes is None or key in colonnes:
-                    if operation == 'chiffrement':
-                        json_dict[key] = aes_encrypt(json_dict[key], cle_chiffrement, mode_chiffrement, iv)
-                    elif operation == 'dechiffrement':
-                        json_dict[key] = aes_decrypt(json_dict[key], cle_chiffrement, mode_chiffrement, iv)
-                    elif operation == 'hashage':
-                        json_dict[key] = sha256_hash(json_dict[key])
-                    else:
-                        print("Opération non reconnue.")
-                        sys.exit(1)
+    elif file_extension == '.xlsx':
+        df = pd.read_excel(fichier_entree, engine='openpyxl')
 
-            # écrire le dictionnaire modifié dans un nouveau fichier json
-            with open(fichier_sortie, 'w') as f:
-                json.dump(json_dict, f)
+        # Appliquer la fonction de decryptage à chaque colonne spécifiée
+        if colonnes:
+            for col in colonnes:
+                if operation == 'chiffrement':
+                    df[col] = df[col].apply(lambda x: aes_encrypt(str(x), cle_chiffrement, mode_chiffrement, iv))
+                elif operation == 'dechiffrement':
+                    df[col] = df[col].apply(lambda x: aes_decrypt(str(x), cle_chiffrement, mode_chiffrement, iv))
+                elif operation == 'hashage':
+                    df[col] = df[col].apply(lambda x: sha256_hash(str(x)))
 
-     elif file_extension == '.xlsx':
-            df = pd.read_excel(fichier_entree, engine='openpyxl')
+        # écrire le dataframe modifié dans un nouveau fichier excel
+        df.to_excel(fichier_sortie, index=False)
 
-            # Appliquer la fonction de decryptage à chaque colonne spécifiée
-            if colonnes:
-                for col in colonnes:
-                    if operation == 'chiffrement':
-                        df[col] = df[col].apply(lambda x: aes_encrypt(str(x), cle_chiffrement, mode_chiffrement, iv))
-                    elif operation == 'dechiffrement':
-                        df[col] = df[col].apply(lambda x: aes_decrypt(str(x), cle_chiffrement, mode_chiffrement, iv))
-                    elif operation == 'hashage':
-                        df[col] = df[col].apply(lambda x: sha256_hash(str(x)))
+    elif file_extension == '.docx':
+        with open(fichier_entree, 'rb') as f:
+            document = f.read()
+        if operation == 'chiffrement':
+            document_encrypted = aes_encrypt(document, cle_chiffrement,mode_chiffrement, iv=None)
+            with open(fichier_sortie, 'wb') as f:
+                f.write(document_encrypted)
+        elif operation == 'dechiffrement':
+            document_decrypted = aes_decrypt(document, cle_chiffrement,mode_chiffrement, iv=None)
+            with open(fichier_sortie, 'wb') as f:
+                f.write(document_decrypted)
+        elif operation == 'hashage':
+            document_hash = sha256_hash(document)
+            with open(fichier_sortie, 'wb') as f:
+                f.write(document_hash.encode())
+    elif file_extension in ['.jpg', '.jpeg', '.png', '.gif']:
+        # Ouvrir le fichier image
+        with open(fichier_entree, 'rb') as f:
+            image = f.read()
 
-            # écrire le dataframe modifié dans un nouveau fichier excel
-            df.to_excel(fichier_sortie, index=False)
-
-     elif file_extension in ['.jpg', '.jpeg', '.png', '.bmp']:
-         # open the image file and read the binary data
-         with open(fichier_entree, 'rb') as file:
-             binary_data = file.read()
-
-         # apply the operation on the binary data
-         if operation == 'chiffrement':
-             encrypted_data = aes_encrypt(binary_data, cle_chiffrement, mode_chiffrement, iv)
-         elif operation == 'dechiffrement':
-             encrypted_data = aes_decrypt(binary_data, cle_chiffrement, mode_chiffrement, iv)
-         elif operation == 'hashage':
-             encrypted_data = sha256_hash(binary_data)
-         else:
-             print("Opération non reconnue.")
-             sys.exit(1)
-         with open(fichier_sortie, 'wb') as encrypted_file:
-             if operation == "chiffrement":
-                 encrypted_file.write(encrypted_data.encode('utf-8'))
-             elif operation == "dechiffrement":
-                 encrypted_file.write(encrypted_data.decode())
-             elif operation == "hashage":
-                 encrypted_file.write(encrypted_data.encode('utf-8'))
-             else:
-                 print("Opération non reconnue.")
-                 sys.exit(1)
-
-     else:
-            print("Type de fichier non reconnu.")
-            sys.exit(1)
+        # Appliquer l'opération sur l'image
+            if operation == 'chiffrement':
+                # Chiffrer l'image avec AES
+                image_encrypted = aes_encrypt(image, cle_chiffrement, mode_chiffrement, iv=None)
+                with open(fichier_sortie, 'wb') as f:
+                    f.write(image_encrypted)
+            elif operation == 'dechiffrement':
+                # Déchiffrer l'image avec AES
+                image_decrypted = aes_decrypt(image, cle_chiffrement, mode_chiffrement, iv=None)
+                with open(fichier_sortie, 'wb') as f:
+                    f.write(image_decrypted)
+            elif operation == 'hashage':
+                # Calculer le hash SHA-256 de l'image
+                image_hash = sha256_hash(image)
+                with open(fichier_sortie, 'wb') as f:
+                    f.write(image_hash.encode())
+            else:
+                print("Opération non reconnue.")
+                sys.exit(1)
+    else:
+        print("Type de fichier non reconnu.")
+        sys.exit(1)
 
 
-encrypt_file(file_extension,fichier_entree,fichier_sortie,cle_chiffrement,mode_chiffrement,vecteur,colonnes=None)
 
+encrypt_file(type,fichier_entree,fichier_sortie,operation,cle_chiffrement,mode_chiffrement,vecteur,colonnes)
 
 end=time.time()
-print("xlsx généré en " + str(end - start) + " secondes")
